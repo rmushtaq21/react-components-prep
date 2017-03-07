6ec09d458c3f7abe7412f5b4d72ac0a20ec861cc
Ramsha Mushtaq <ramsha.a.mushtaq@gmail.com>
2017-03-07 10:50:34 -0800

    Create a grocery list component

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
-// TODO
+var GroceryList = (props) => (
+  <div>
+  <h1>Grocery List</h1>
+    <ul>
+      <li>Apples</li>
+      <li>Bananas</li>
+      <li>Cilantro</li>
+      <li>Doughnut</li>
+      <Jalepenos/>
+      <Kale/>
+    </ul>
+  </div>
+);
+
+var Kale = () => (
+  <li>Kale</li>
+);
+
+var Jalepenos = () => (
+  <li>Jalepenos</li>
+);
+
+// var ListItems = () => (
+//   <div>
+//     <h1>Shopping List</h1>
+//     <GroceryList listItem={['Apples', 'Bananas', 'Cellery']}/>
+//   </div>
+// );
+
+
+ReactDOM.render(<GroceryList />, document.getElementById('app'));
